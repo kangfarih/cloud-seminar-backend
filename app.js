4d41a8cb6236a18a10d93f977cea245a08a090dc
Farih Muhammad <farihul.ibad@gmail.com>
2021-02-09 08:53:37 +0700

    Update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const port = Number(Process.env.PORT || 8787);
+const port = process.env.PORT || 8787;
 const mongoose = require('mongoose');
 
 // setup express
