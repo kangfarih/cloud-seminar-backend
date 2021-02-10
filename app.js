70a0e084b5d96b50ccfb082b694916f0e99c3e10
Farih Muhammad <farihul.ibad@gmail.com>
2021-02-10 17:11:53 +0700

    Revert "initial setup express rest api with boilerplate from https://github.com/danielfsousa/express-rest-es2017-boilerplate"
    This reverts commit cf56535a5c2644e7f4ca4e9d3deb933c995b235b.

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const port = process.env.PORT || 80 || 8787;
+const mongoose = require('mongoose');
+
+// setup express
+const app = express();
+
+// connect to mongodb
+
+mongoose.connect('mongodb+srv://adm-seminar:4dmSeminar@cluster0.wdfb4.mongodb.net/cloud-seminar?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+mongoose.Promise = global.Promise;
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use('/api', require('./routes/api'));
+
+// error handling middleware
+app.use(function (err, req, res, next) {
+    console.log(err); // to see properties of message in our console
+    res.status(422).send({
+        error: err.message
+    });
+});
+
+app.listen(port, () => {
+    console.log(`App is listening at ${port}`);
+})
