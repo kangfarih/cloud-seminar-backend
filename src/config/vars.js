7ef0de8f2071d4b1360482bf5c039f5e9c452070
Farih Muhammad <farihul.ibad@gmail.com>
2021-02-10 17:11:39 +0700

    Revert "update"
    This reverts commit 502eb3526e5561df0b78400ee8d1fc3980b08038.

--- src/config/vars.js
+++ src/config/vars.js
@@ -8,7 +8,7 @@ require('dotenv-safe').load({
 
 module.exports = {
   env: process.env.NODE_ENV,
-  port: process.env.PORT || 8787,
+  port: process.env.PORT,
   jwtSecret: process.env.JWT_SECRET,
   jwtExpirationInterval: process.env.JWT_EXPIRATION_MINUTES,
   mongo: {
